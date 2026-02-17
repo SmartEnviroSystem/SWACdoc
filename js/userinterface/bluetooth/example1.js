7312c740aafdd6f7dadeda0f41ac359dfee266b2
FlorianFehring <florian.fehring@hsbi.de>
2026-02-17 08:41:12 +0100

    Added: Bluetooth: Inital comit for new component

---
+++ js/userinterface/bluetooth/example1.js
@@ -0,0 +1,8 @@
+window["bluetooth_example1_conf_options"] = {
+    filterDevices: [
+        {namePrefix: "Sensor"},
+        {namePrefix: "Sensor"},
+        {namePrefix: "sensor"},
+        {namePrefix: "SENSOR"}
+    ]
+};
