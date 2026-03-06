bf458db8fa010dbd2baf8b6729387081921e59a9
Maczap99 <90683916+Maczap99@users.noreply.github.com>
2026-03-06 09:15:04 +0100

    edit (SWACdoc, dashboard): fix dashboard example

--- js/userinterface/dashboard/index.js
+++ js/userinterface/dashboard/index.js
@@ -1,12 +1,3 @@
 window['explaincomponent_options'] = {
     componentName: 'Dashboard'
 };
-
-window["dashboard_example1_options"] = {
-    usersettingsRequestor: {
-        fromName: '../../data/userinterface/dashboard/cards_usersettings.json',
-        fromWheres: {
-            filter: 'user_id,eq,{user_id}'
-        }
-    }
-};
