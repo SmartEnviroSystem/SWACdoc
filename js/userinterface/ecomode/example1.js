ef8c8c1c867d3ad24d4ed4fc5670056510a33e35
FlorianFehring <florian.fehring@hsbi.de>
2026-03-21 12:47:18 +0100

    Changed: Ecomode: Reorganised example

--- js/userinterface/ecomode/example1.js
+++ js/userinterface/ecomode/example1.js
@@ -1,10 +1,6 @@
-window['explaincomponent_options'] = {
-    componentName: 'Ecomode'
-};
-
 window["dashboard_example1_options"] = {
     usersettingsRequestor: {
-        fromName: '../../data/userinterface/dashboard/cards_usersettings.json',
+        fromName: '../../../data/userinterface/ecomode/example1/cards_usersettings.json',
         fromWheres: {
             filter: 'user_id,eq,{user_id}'
         }
