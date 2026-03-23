8f1a7ce2c3160a4e76859cffe0a5cdf636176440
FlorianFehring <florian.fehring@hsbi.de>
2026-03-23 13:37:34 +0100

    Changed: Calculate: Updated documentation

---
+++ js/visualisation/calculate/example1.js
@@ -0,0 +1,8 @@
+window['calculate_example1_options'] = {
+    calculations: [
+        {
+            formular: "u * i",  // Any JS formular
+            target: "p"         // Name of the target attribute
+        }
+    ]
+};
---
+++ js/visualisation/calculate/example2.js
@@ -0,0 +1,9 @@
+window['calculate_example2_options'] = {
+    sourceattr: 'source', // Name of the attribute that contains an URL to the datasets
+    calculations: [
+        {
+            formular: "voltage * current",
+            target: "yield"
+        }
+    ]
+};
