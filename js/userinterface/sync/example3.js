abe273ee87e59b5d6409cf62816ce1afb5f0e483
FlorianFehring <florian.fehring@hsbi.de>
2026-03-23 13:41:10 +0100

    Changed: Sync: Updated documentation

--- js/userinterface/sync/example3.js
+++ js/userinterface/sync/example3.js
@@ -1,10 +1,11 @@
 window['sync_example3_options']={
-    syncTarget: '/SmartDataTest/smartdata/records/target_table?storage=smartmonitoring',
     // Name of the attribute that indicates the snync state
     syncedFlagAttribute: 'synced',
     syncModeRequestor: {
         fromName: 'userinterface/sync/example3/syncmode.json'
     },
+    syncStateAttribute: 'status',
+    syncStateRunningValue: 'running',
     syncStartRequestor: {
         fromName: 'userinterface/sync/example3/syncmodetoggle.json'
     },
