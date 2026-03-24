905ee5394549d381aecb1669df76935591c7ca42
FlorianFehring <florian.fehring@hsbi.de>
2026-03-24 08:08:26 +0100

    Changed: Select: Replaced callback functions with events

--- js/input/select/example6.js
+++ js/input/select/example6.js
@@ -1,6 +1,6 @@
 let onChangeFunc = function (evt) {
     // You are inside the component here
-    let inputs = this.getInputs();
+    let inputs = evt.detail.requestor.getInputs();
     let msg = 'You selected:';
     for (let curInput of inputs) {
         msg += ' ' + curInput.name + ' = ' + curInput.value;
@@ -8,7 +8,7 @@ let onChangeFunc = function (evt) {
     alert(msg);
 };
 
-window['select_example6_select_options'] = {onChange: onChangeFunc};
-window['select_example6_multiselect_options'] = {onChange: onChangeFunc};
-window['select_example6_checkboxes_options'] = {onChange: onChangeFunc};
-window['select_example6_datalist_options'] = {onChange: onChangeFunc};
+document.addEventListener('swac_select_example6_select_selection_changed',onChangeFunc);
+document.addEventListener('swac_select_example6_multiselect_selection_changed',onChangeFunc);
+document.addEventListener('swac_select_example6_checkboxes_selection_changed',onChangeFunc);
+document.addEventListener('swac_select_example6_datalist_selection_changed',onChangeFunc);
