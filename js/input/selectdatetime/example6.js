889c320795ad32e6dd3c336e91c506041257de05
FlorianFehring <florian.fehring@hsbi.de>
2026-03-25 06:47:49 +0100

    Added: Selectdatetime: Template with labeld input

--- js/input/selectdatetime/example6.js
+++ js/input/selectdatetime/example6.js
@@ -1,13 +1,13 @@
 window.onload = function () {
-    let example5Btn = document.querySelector('#selectdatetime_example5btn');
+    let example6Btn = document.querySelector('#selectdatetime_example6btn');
     // Avoid error if example is commented out
-    if (!example5Btn)
+    if (!example6Btn)
         return;
-    example5Btn.addEventListener('click', function (evt) {
-        let dtElem = document.querySelector('#selectdatetime_example5');
-
+    example6Btn.addEventListener('click', function (evt) {
+        let dtElem = document.querySelector('#selectdatetime_example6');
         let data = dtElem.swac_comp.getData()[dtElem.swac_comp.options.fromName];
         let set = data.getSet(1);
+        console.log('First set',set);
         // For example static implemented with given name of source and hardcoded dateset id (1)
         alert('The actual inputted date / time is: ' + set.datetime);
     });
