02bcb17a9f190a6b855bd5c8fa0e91084b73b466
FlorianFehring <florian.fehring@hsbi.de>
2026-09-17 11:45:36 +0200

    Added: Present: DatanAnalysisSPL

---
+++ js/visualisation/present/example16.js
@@ -0,0 +1,9 @@
+window['present_example16_options'] = {
+    showSidebar: true,
+    plugins: new Map([
+        ['DatasetAnalysis', {
+            id: 'DatasetAnalysis',
+            active: true
+        }]
+    ])
+};
